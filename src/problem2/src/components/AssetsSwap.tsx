3162ced4c26bf37e624f7a5150a66b9b7dedccf9
Quang Tuyen Dinh <qtuyen43@gmail.com>
2025-11-22 07:40:18 +0100

    feat: init assets management

---
+++ src/problem2/src/components/AssetsSwap.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./AssetsSwap.scss"
+
+function AssetsSwap() {
+  return (
+    <div className="assets-swap-container">
+      
+    </div>
+  )
+}
+
+export default AssetsSwap
---
+++ src/problem2/src/pages/AssetsManagement.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AssetsSwap from '../components/AssetsSwap'
+
+function AssetsManagement() {
+  return (
+    <main>
+      <section>
+        <AssetsSwap />
+      </section>
+    </main>
+  )
+}
+
+export default AssetsManagement
